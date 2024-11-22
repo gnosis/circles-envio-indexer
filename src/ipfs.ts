ffad769d666526ef9f38948f98a95bd6b7983919
Johanna Fransson <adrina0002@gmail.com>
2024-11-22 15:01:58 +0700

    feat: add centralized endpoint as well which uses private IPFS gateway

--- src/ipfs.ts
+++ src/ipfs.ts
@@ -1,34 +1,3 @@
-/* export async function getProfileMetadataFromIpfs(
-  metadataDigest: string
-): Promise<{ cidV0: string; data: Profile | null } | null> {
-  if (!metadataDigest) {
-    return null;
-  }
-  const slicedDigest = metadataDigest.slice(2, metadataDigest.length);
-
-  const cache = await ProfileCache.init();
-  const cacheResult = await cache.read(slicedDigest);
-
-  if (cacheResult) {
-    return cacheResult;
-  }
-
-  const cidV0 = uint8ArrayToCidV0(
-    Uint8Array.from(Buffer.from(slicedDigest, "hex"))
-  );
-
-  const externalResponse = await fetch(`https://ipfs.io/ipfs/${cidV0}`);
-  const externalData = await externalResponse.json();
-
-  if (!externalData) {
-    return { cidV0, data: null };
-  }
-
-  await cache.add(slicedDigest, cidV0, externalData as Profile);
-  return { cidV0, data: externalData as Profile };
-}
- */
-
 import multihash from "multihashes";
 import { Profile } from "./types";
 import { ProfileCache } from "./cache";
@@ -42,9 +11,11 @@ const config = {
 };
 
 const IPFS_ENDPOINTS = [
-  "https://cloudflare-ipfs.com/ipfs",
+  "https://rpc.aboutcircles.com/profiles/get",
+
+  /*   "https://cloudflare-ipfs.com/ipfs",
   "https://ipfs.io/ipfs",
-  "https://gateway.pinata.cloud/ipfs",
+  "https://gateway.pinata.cloud/ipfs", */
 ];
 
 async function validateImage(imageUrl: string): Promise<boolean> {
@@ -69,7 +40,11 @@ async function fetchFromEndpoint(
 ): Promise<{ data: Profile | null; timeTaken: number }> {
   const startTime = performance.now();
   try {
-    const response = await fetch(`${endpoint}/${cidV0}`);
+    const url = endpoint.includes("aboutcircles.com")
+      ? `${endpoint}?cid=${cidV0}`
+      : `${endpoint}/${cidV0}`;
+
+    const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }

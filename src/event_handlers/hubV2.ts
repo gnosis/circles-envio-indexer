668fc9742b16b11e898f02cf4c6418036e0a6c82
Bernardo <bernardo.vieira@gnosis.io>
2024-11-26 11:01:02 +0000

    feat: from & to as Avatar types

--- src/event_handlers/hubV2.ts
+++ src/event_handlers/hubV2.ts
@@ -270,10 +270,10 @@ HubV2.StreamCompleted.handlerWithLoader({
     return {
       transfers,
       demurrageFrom: transfers.filter(
-        (t) => t.to === zeroAddress && t.from === event.params.from
+        (t) => t.to_id === zeroAddress && t.from_id === event.params.from
       ),
       demurrageTo: transfers.filter(
-        (t) => t.to === zeroAddress && t.from === event.params.to
+        (t) => t.to_id === zeroAddress && t.from_id === event.params.to
       ),
     };
   },
@@ -295,8 +295,8 @@ HubV2.StreamCompleted.handlerWithLoader({
       transactionIndex: event.transaction.transactionIndex,
       transactionHash: event.transaction.hash,
       logIndex: event.logIndex,
-      from: event.params.from,
-      to: event.params.to,
+      from_id: event.params.from,
+      to_id: event.params.to,
       operator: undefined,
       value: event.params.amounts.reduce((a, b) => a + b, 0n),
       // TODO: fix - this isn't always true, since it can be multiple tokens
@@ -322,7 +322,7 @@ HubV2.TransferSingle.handlerWithLoader({
     return {
       avatar,
       demurrageTransfer: transfers.filter(
-        (t) => t.to === zeroAddress && t.from === event.params.to
+        (t) => t.to_id === zeroAddress && t.from_id === event.params.to
       ),
     };
   },
@@ -372,7 +372,7 @@ HubV2.DiscountCost.handlerWithLoader({
       avatarBalance,
       demurrageTransfer: transfers
         .sort((a, b) => a.logIndex - b.logIndex)
-        .filter((t) => t.to === zeroAddress && t.from === event.params.account)
+        .filter((t) => t.to_id === zeroAddress && t.from_id === event.params.account)
         .at(-1),
     };
   },
